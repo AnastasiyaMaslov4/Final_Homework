8ea09ea3ec99c8da0c667a324a665dc6349b368d
AnastasiyaM <anastasiya.smirnova96@yandex.ru>
2022-09-07 12:14:11 +0300

    Добавлена папка с программой

---
+++ programm/Program.cs
@@ -0,0 +1,16 @@
+﻿ 
+ string[] stringArray;
+ stringArray = new string[4];
+ Console.WriteLine("Введите 4 строки:");
+
+stringArray[0] = Console.ReadLine();
+stringArray[1] = Console.ReadLine();
+stringArray[2] = Console.ReadLine();
+stringArray[3] = Console.ReadLine();
+
+
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3) Console.Write($"{stringArray[i]}; ");
+}
+
